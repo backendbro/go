85e15deb38e9028f8f1f6dce6fc3daaecc1d43b1
backendbro <ukagha.nzubechukwu@yahoo.com>
2026-05-22 12:05:20 +0100

    I am done with concurrency, where I learnt what gorountines are, I learnt how Go hates race conditons and how to fix thm using go channels

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,18 +1,25 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultsChannel := make(chan result)
 
 	for _, url := range urls {
-		go func() {
-			results[url] = wc(url)
-		}()
+		go func(u string) {
+			resultsChannel <- result{u, wc(u)}
+		}(url)
 	}
 
-	time.Sleep(5 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		r := <-resultsChannel
+		results[r.string] = r.bool
+	}
+
 	return results
 }
