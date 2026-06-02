c0c501a5252304a90a57967be286d80e3a42368a
backendbro <ukagha.nzubechukwu@yahoo.com>
2026-06-02 12:30:02 +0100

    Started off with reflection.

---
+++ reflection/reflection_test.go
@@ -0,0 +1,53 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk1(t *testing.T) {
+	expected := "Chris"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if got[0] != expected {
+		t.Errorf("got %q, want %q", got[0], expected)
+	}
+}
+
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
